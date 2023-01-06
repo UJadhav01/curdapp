9f619b3467646f492bd99d275f8fd6ffca046028
UJadhav01 <111181029+UJadhav01@users.noreply.github.com>
2023-01-06 08:24:23 +0530

    app.component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'curdapp'`, () => {
+  it(`should have as title 'angular-14-firebase-crud'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('curdapp');
+    expect(app.title).toEqual('angular-14-firebase-crud');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('curdapp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-14-firebase-crud app is running!');
   });
 });
