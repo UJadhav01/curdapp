9c0d7f88b563ef0ce7fecbe30468dd6eef52ffbd
UJadhav01 <111181029+UJadhav01@users.noreply.github.com>
2023-01-06 08:22:31 +0530

    karma.conf

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/curdapp'),
+      dir: require('path').join(__dirname, './coverage/angular-14-firebase-crud'),
       subdir: '.',
       reporters: [
         { type: 'html' },
