8cfa0f6dcf6b02fc6affc563671661b0790a1239
UJadhav01 <111181029+UJadhav01@users.noreply.github.com>
2023-01-06 08:23:57 +0530

    app-routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddStudentComponent } from './components/add-student/add-student.component';
+import { StudentListComponent } from './components/student-list/student-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'student-records', pathMatch: 'full' },
+  { path: 'student-records', component: StudentListComponent },
+  { path: 'add-student', component: AddStudentComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
